1f3a0b1a5eb3054ee75b86b3fefc56c9446ce003
xinyuan.yao <yao.ntno@gmail.com>
2020-07-07 13:25:50 +0000

    0.646

--- app/dataset/wheat.py
+++ app/dataset/wheat.py
@@ -80,12 +80,10 @@ class WheatDataset(Dataset):
         self.train_transforms = A.Compose(
             [
                 #  A.RandomResizedCrop(p=1, height=max_size, width=max_size),
-                A.RGBShift(),
                 A.VerticalFlip(),
                 A.RandomRotate90(),
                 A.HorizontalFlip(),
                 A.RandomBrightnessContrast(p=1),
-                A.CLAHE(p=1),
                 A.GaussNoise(),
             ],
             bbox_params=bbox_params,
