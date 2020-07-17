e863d5534249b0d93e1764bb98828f392f3621ac
xinyuan.yao <yao.ntno@gmail.com>
2020-07-17 16:27:38 +0000

    keep test

--- experiments/ctdtrv1/ctdtrv1/config.py
+++ experiments/ctdtrv1/ctdtrv1/config.py
@@ -10,7 +10,7 @@ annot_file = "/kaggle/input/global-wheat-detection/train.csv"
 
 n_splits = 5
 fold_idx = 0
-lr = 1e-4
+lr = 5e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
 metric:Tuple[str, Literal['max', 'min']] = ("score", "max")
 max_size = 512
@@ -19,16 +19,16 @@ num_workers = 8
 
 effdet_id:Phi = 4
 depth = 1
-out_idx: PyramidIdx = 4
+out_idx: PyramidIdx = 5
 channels = 128
 
 sigma = 5.0
 heatmap_weight = 1.0
-box_weight = 40.0
-iou_threshold = 0.55
+box_weight = 1.0
+iou_threshold = 0.50
 use_peak = True
 
-confidence_threshold = 0.30
+confidence_threshold = 0.32
 final_threshold = 0.0
 
 fpn_depth = 1
